0816073049153e27d48b3f28ddce00125f3020f8
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2022-09-15 12:12:52 +0600

    Add files via upload

---
+++ DataTransferService/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DataTransferService
+{
+    /// https://stackoverflow.com/questions/13019433/calling-a-method-every-x-minutes
+    /// 
+
+    class Program
+    {
+        ///log4net: https://stackify.com/log4net-guide-dotnet-logging/
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        ///show/hide cmd  https://stackoverflow.com/questions/3571627/show-hide-the-console-window-of-a-c-sharp-console-application      
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        static void Main(string[] args)
+        {
+           
+
+            //log.Info("Hello logging world!   Info");
+            //log.Debug("Hello logging world!   Debug");
+            //log.Warn("Hello logging world!   Warn");
+            //log.Error("Hello logging world!  Error");
+            //log.Fatal("Hello logging world!  Fatal");
+            
+
+            var handle = GetConsoleWindow();
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+            // Show
+            //ShowWindow(handle, SW_SHOW);
+
+
+
+            //Console.WriteLine("*************** Wellcome  Data Transfer Service. Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "***************");
+            log.Info("*************** Welcome  Data Transfer Service. Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "***************");
+            var time = ConfigurationManager.AppSettings["excuteDuration"];
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(5);
+            try
+            {
+                periodTimeSpan = TimeSpan.FromMinutes(Convert.ToDouble(time));
+            }
+            catch (Exception exp)
+            {                
+                //Console.Write(exp.Message + " \n");
+                log.Error(exp.Message);
+            }
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+
+                /// create service instance
+                Service sale = new Service();
+                SummaryService summary = new SummaryService();
+
+                /// get data from source db
+                DataTable saleDt = sale.GetData();
+                DataTable summryDt = summary.GetData();
+
+                /// save sale data data
+                /// 
+                bool isSave = false;
+                if (saleDt.Rows.Count>0)
+                {
+                    isSave = sale.SetData(saleDt);
+                }
+                else
+                {
+                    //Console.WriteLine("New Sale Data Not Found at "+DateTime.Now.ToShortTimeString());
+                    log.Info("New Sale Data Not Found at " + DateTime.Now.ToShortTimeString());
+                }                
+                if (isSave)
+                {
+                    sale.UpdateData();
+                }
+
+                /// save summary data data
+                /// 
+                bool isSaveSummary = false;
+                if (summryDt.Rows.Count > 0)
+                {
+                    isSaveSummary = summary.SetData(summryDt);
+                }
+                else
+                {
+                    //Console.WriteLine("New Summary Data Not Found at " + DateTime.Now.ToShortTimeString());
+                    log.Info("New Summary Data Not Found at " + DateTime.Now.ToShortTimeString());
+                }
+                if (isSaveSummary)
+                {
+                    summary.UpdateData();
+                }
+
+            }, null, startTimeSpan, periodTimeSpan);
+
+
+
+            Console.Read();
+
+            /// run-> shell:startup then paste exe shortcut for auto run when pc start
+        }
+    }
+}
---
+++ DataTransferService/Service.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataTransferService
+{
+    class Service
+    {
+
+        ///log4net: https://stackify.com/log4net-guide-dotnet-logging/
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        string from_connection = ConfigurationManager.ConnectionStrings["FROM_DB_CONN"].ConnectionString;
+        string to_connection = ConfigurationManager.ConnectionStrings["TO_DB_CONN"].ConnectionString;
+        [STAThread]
+        public DataTable GetData()
+        {   
+            DataTable dataTable = new DataTable();
+            SqlConnection conn = new SqlConnection(from_connection);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    cmd.CommandText = "SELECT * FROM dbo.Sale" + DateTime.Now.Year + DateTime.Now.Month.ToString("00") + "_Processed WHERE isSync IS NULL OR isSync = 0";
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.SelectCommand.CommandTimeout = 0;
+                    da.Fill(dataTable);
+
+                    //Console.WriteLine(dataTable.Rows.Count + " Sale Data found ");
+                    log.Info(dataTable.Rows.Count + " Sale Data found ");
+                   
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message );
+                log.Error(exp.Message);
+                
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }
+
+
+
+
+        [STAThread]
+        public bool SetData(DataTable dt)
+        {
+            string debugQuery = "";
+            bool res = false;
+            SqlConnection conn = new SqlConnection(to_connection);
+            try
+            {
+                int count = 1;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(@" INSERT INTO [dbo].[Sale" + DateTime.Now.Year + DateTime.Now.Month.ToString("00") + "] ([CmpIDX] ,[Invoice] ,[SaleDT] ,[sBarCode] ,[BarCode] ,[SKU] ,[SupID] ,[SupName] ,[GroupName] ,[PrdName] ,[BTName] ,[SSName] ,[Qty] ,[CPU] ,[RPU] ,[wsp] ,[wsq] ,[SQty] ,[rQty] ,[rAmt] ,[cInvoice] ,[ShopID] ,[PayType] ,[Discount] ,[DiscAmt] ,[DiscAmtPrd] ,[VAT] ,[PrdSlNo] ,[UserID] ,[CardName] ,[CardNo] ,[CounterID] ,[PrvCusID] ,[CusName] ,[VATPrcnt] ,[DiscPrcnt] ,[Returned] ,[Flag] ,[MrCode] ,[Point] ,[TSEC] ,[Posted] ,[Ref] ,[IsTransfer] ) VALUES ");
+                        sb.Append("( '" + row["CmpIDX"].ToString() + "',");
+                        sb.Append(" '" + row["Invoice"].ToString() + "',");
+                        sb.Append(" '" + row["SaleDT"].ToString() + "',");
+                        sb.Append(" '" + row["sBarCode"].ToString() + "',");
+                        sb.Append(" '" + row["BarCode"].ToString() + "',");
+                        sb.Append(" '" + row["SKU"].ToString() + "',");
+                        sb.Append(" '" + row["SupID"].ToString() + "',");
+                        sb.Append(" '" + row["SupName"].ToString() + "',");
+                        sb.Append(" '" + row["GroupName"].ToString() + "',");
+                        sb.Append(" '" + row["PrdName"].ToString() + "',");
+                        sb.Append(" '" + row["BTName"].ToString() + "',");
+                        sb.Append(" '" + row["SSName"].ToString() + "',");
+                        sb.Append(" '" + row["Qty"].ToString() + "',");
+                        sb.Append(" '" + row["CPU"].ToString() + "',");
+                        sb.Append(" '" + row["RPU"].ToString() + "',");
+                        sb.Append(" '" + row["wsp"].ToString() + "',");
+                        sb.Append(" '" + row["wsq"].ToString() + "',");
+                        sb.Append(" '" + row["SQty"].ToString() + "',");
+                        sb.Append(" '" + row["rQty"].ToString() + "',");
+                        sb.Append(" '" + row["rAmt"].ToString() + "',");
+                        sb.Append(" '" + row["cInvoice"].ToString() + "',");
+                        sb.Append(" '" + row["ShopID"].ToString() + "',");
+                        sb.Append(" '" + row["PayType"].ToString() + "',");
+                        sb.Append(" '" + row["Discount"].ToString() + "',");
+                        sb.Append(" '" + row["DiscAmt"].ToString() + "',");
+                        sb.Append(" '" + row["DiscAmtPrd"].ToString() + "',");
+                        sb.Append(" '" + row["VAT"].ToString() + "',");
+                        sb.Append(" '" + row["PrdSlNo"].ToString() + "',");
+                        sb.Append(" '" + row["UserID"].ToString() + "',");
+                        sb.Append(" '" + row["CardName"].ToString() + "',");
+                        sb.Append(" '" + row["CardNo"].ToString() + "',");
+                        sb.Append(" '" + row["CounterID"].ToString() + "',");
+                        sb.Append(" '" + row["PrvCusID"].ToString() + "',");
+                        sb.Append(" '" + row["CusName"].ToString() + "',");
+                        sb.Append(" '" + row["VATPrcnt"].ToString() + "',");
+                        sb.Append(" '" + row["DiscPrcnt"].ToString() + "',");
+                        sb.Append(" '" + row["Returned"].ToString() + "',");
+                        sb.Append(" '" + row["Flag"].ToString() + "',");
+                        sb.Append(" '" + row["MrCode"].ToString() + "',");
+                        sb.Append(" '" + row["Point"].ToString() + "',");
+                        sb.Append(" '" + row["TSEC"].ToString() + "',");
+                        sb.Append(" '" + row["Posted"].ToString() + "',");
+                        sb.Append(" '" + row["Ref"].ToString() + "',");
+                        sb.Append(" '" + row["IsTransfer"].ToString() + "') ");
+                        cmd.CommandText = sb.ToString();
+                        debugQuery = sb.ToString();
+                        cmd.ExecuteScalar();
+                        //Console.WriteLine(count++);
+                        log.Info(count++);
+                    }
+                    
+                  
+                    res = true;
+                    //Console.WriteLine(dt.Rows.Count + " data succesfully inserted at " + DateTime.Now.ToShortTimeString() );
+                    log.Info(dt.Rows.Count + " data succesfully inserted at " + DateTime.Now.ToShortTimeString());
+
+                }
+            }
+            catch (Exception exp)
+            {
+                res = false;
+                //Console.WriteLine(exp.Message );
+                log.Info(exp.Message);
+                //Console.WriteLine(debugQuery);
+                log.Info(debugQuery);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return res;
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }
+
+
+
+        [STAThread]
+        public void UpdateData()
+        {
+
+            
+            SqlConnection conn = new SqlConnection(from_connection);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(@" UPDATE [dbo].[Sale" + DateTime.Now.Year + DateTime.Now.Month.ToString("00") + "_Processed] SET isSync = 1 WHERE isSync IS NULL OR isSync = 0 ");                    
+                    cmd.CommandText = sb.ToString();
+                    cmd.ExecuteScalar();
+                    //Console.WriteLine("Succesfully Selected \n");
+
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message );
+                log.Error(exp.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }  
+
+    }
+
+
+    class SummaryService
+    {
+
+
+        ///log4net: https://stackify.com/log4net-guide-dotnet-logging/
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+
+        string from_connection = ConfigurationManager.ConnectionStrings["FROM_DB_CONN"].ConnectionString;
+        string to_connection = ConfigurationManager.ConnectionStrings["TO_DB_CONN"].ConnectionString;
+        [STAThread]
+        public DataTable GetData()
+        {            
+            DataTable dataTable = new DataTable();
+            SqlConnection conn = new SqlConnection(from_connection);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    cmd.CommandText = "SELECT * FROM dbo.SSummary_Processed WHERE isSync IS NULL OR isSync = 0";
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.SelectCommand.CommandTimeout = 0;
+                    da.Fill(dataTable);
+
+                    //Console.WriteLine(dataTable.Rows.Count + " Summary Data found ");
+                    log.Info(dataTable.Rows.Count + " Summary Data found ");
+
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message + " \n");
+                log.Error(exp.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataTable;
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }
+
+
+
+
+        [STAThread]
+        public bool SetData(DataTable dt)
+        {
+            string debugQuery = "";
+            bool res = false;            
+            SqlConnection conn = new SqlConnection(to_connection);
+            try
+            {
+                
+                int count = 1;
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                   
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(@" INSERT INTO [dbo].[SSummary] ([Invoice],[SaleDT],[TotalCost],[TotalAmt],[Discount],[DiscAmt],[VAT],[NetAmt],[CshAmt],[CrdAmt],[PayType],[UserID],[ShopID],[CardName],[CardNo],[CounterID],[PrvCusID],[CusName],[ReturnedAmt],[rTotalCost],[rVATAmt],[rDiscAmt],[rTotalAmt],[cInvoice],[ReturnedType],[Flag],[PaidAmt],[ChangeAmt],[Point],[MrCode],[TSEC],[advamt],[advslip],[Ref],[RefNO],[DiscType] ) VALUES ");
+
+                        sb.Append("( '" + row["Invoice"].ToString() + "',");
+                        sb.Append(" '" + row["SaleDT"].ToString() + "',");
+                        sb.Append(" '" + row["TotalCost"].ToString() + "',");
+                        sb.Append(" '" + row["TotalAmt"].ToString() + "',");
+                        sb.Append(" '" + row["Discount"].ToString() + "',");
+                        sb.Append(" '" + row["DiscAmt"].ToString() + "',");
+                        sb.Append(" '" + row["VAT"].ToString() + "',");
+                        sb.Append(" '" + row["NetAmt"].ToString() + "',");
+                        sb.Append(" '" + row["CshAmt"].ToString() + "',");
+                        sb.Append(" '" + row["CrdAmt"].ToString() + "',");
+                        sb.Append(" '" + row["PayType"].ToString() + "',");
+                        sb.Append(" '" + row["UserID"].ToString() + "',");
+                        sb.Append(" '" + row["ShopID"].ToString() + "',");
+                        sb.Append(" '" + row["CardName"].ToString() + "',");
+                        sb.Append(" '" + row["CardNo"].ToString() + "',");
+                        sb.Append(" '" + row["CounterID"].ToString() + "',");
+                        sb.Append(" '" + row["PrvCusID"].ToString() + "',");
+                        sb.Append(" '" + row["CusName"].ToString() + "',");
+                        sb.Append(" '" + row["ReturnedAmt"].ToString() + "',");
+                        sb.Append(" '" + row["rTotalCost"].ToString() + "',");
+                        sb.Append(" '" + row["rVATAmt"].ToString() + "',");
+                        sb.Append(" '" + row["rDiscAmt"].ToString() + "',");
+                        sb.Append(" '" + row["rTotalAmt"].ToString() + "',");
+                        sb.Append(" '" + row["cInvoice"].ToString() + "',");
+                        sb.Append(" '" + row["ReturnedType"].ToString() + "',");
+                        sb.Append(" '" + row["Flag"].ToString() + "',");
+                        sb.Append(" '" + row["PaidAmt"].ToString() + "',");
+                        sb.Append(" '" + row["ChangeAmt"].ToString() + "',");
+                        sb.Append(" '" + row["Point"].ToString() + "',");
+                        sb.Append(" '" + row["MrCode"].ToString() + "',");
+                        sb.Append(" '" + row["TSEC"].ToString() + "',");
+                        sb.Append(" '" + row["advamt"].ToString() + "',");
+                        sb.Append(" '" + row["advslip"].ToString() + "',");
+                        sb.Append(" '" + row["Ref"].ToString() + "',");
+                        sb.Append(" '" + row["RefNO"].ToString() + "',");
+                        sb.Append(" '" + row["DiscType"].ToString() + "')");                        
+                        cmd.CommandText = sb.ToString();
+                        debugQuery = sb.ToString();
+                        cmd.ExecuteScalar();
+                        //Console.WriteLine(count++);
+                        log.Info(count++);
+                    }
+                          
+                    res = true;
+                    //Console.WriteLine(dt.Rows.Count + " data succesfully inserted at "+ DateTime.Now.ToShortTimeString() );
+                    log.Info(dt.Rows.Count + " data succesfully inserted at " + DateTime.Now.ToShortTimeString());
+
+                }
+            }
+            catch (Exception exp)
+            {
+                res = false;
+                //Console.WriteLine(exp.Message );
+                log.Error(exp.Message);
+                //Console.WriteLine( debugQuery);
+                log.Info(debugQuery);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return res;
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }
+
+
+
+        [STAThread]
+        public void UpdateData()
+        {
+            SqlConnection conn = new SqlConnection(from_connection);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandTimeout = 0;
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(@" UPDATE [dbo].[SSummary_Processed] SET isSync = 1 WHERE isSync IS NULL OR isSync = 0 ");
+                    cmd.CommandText = sb.ToString();
+                    cmd.ExecuteScalar();
+                    //Console.WriteLine("Succesfully Selected \n");
+
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message );
+                log.Error(exp.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            //Console.WriteLine("\n\n Press any key to quite");
+            //Console.Read();
+        }
+
+    }
+
+}
